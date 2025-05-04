0ffc7a36a14d18795621a3cdace9db41c0233580
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-04 17:37:02 +0200

    Creation du formulaire de connexion et d'inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    'path' : '',
+    'component' : HomeComponent
+  },
+  {
+    'path' : 'register',
+    'component' : RegisterComponent
+  },
+  {
+    'path' : 'login',
+    'component' : LoginComponent
+  },
+  {
+    'path' : '**',
+    'component' : NotFoundComponent
+  }
+];
