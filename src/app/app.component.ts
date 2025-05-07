b60978ea71d3897ad085eea9065156ff82ed7a18
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-07 11:26:18 +0200

    ajout d'un composant pour la navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,LoginComponent,RegisterComponent],
+  imports: [RouterOutlet, RouterLink, LoginComponent, RegisterComponent, FooterComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
